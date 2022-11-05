2305556fe3f29a837259911430958eeb6be3677f
Samuel Khongthaw <skthaw1@gmail.com>
2022-11-05 07:44:51 +0530

    changed run config

--- models/user.js
+++ models/user.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema(
     {
-
+        name: String,
+        password: String,
     }
 );
+
+module.exports = mongoose.model('Task', UserSchema);
